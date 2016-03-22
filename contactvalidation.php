d3c65aa233e28835f2b58d6c57986b2b1d1b7682
erica dreisbach <erica@ericadreisbach.com>
2016-03-22 12:55:39 -0500

    back to what worked before

--- contactvalidation.php
+++ contactvalidation.php
@@ -85,16 +85,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $allcaps = $_REQUEST['allcaps'] ;
   $clientproject = $_REQUEST['clientproject'] ;
 
-  $message = 'From: ' . $name . ' ' . $email . '\r\n';
+  $message = 'From: $name $email \r\n';
   $message .= 'Prior experience with developers: ' . $experience . '\r\n';
   $message .= 'Ok to send an alll caps email: ' . $allcaps . '\r\n';
   $message .= $clientproject;
 
-  $headers  = 'MIME-Version: 1.0' . '\r\n';
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . '\r\n';
-  $headers .= 'From: $name <$email>';
-
-  mail("erica@ericadreisbach.com", "web design/development", $message, $headers);
+  mail("erica@ericadreisbach.com", "web design/development", $message, "From: $name <$email>");
 
   echo "<style type='text/css'>#contact>.wrapper>.-successhide{display: none;}</style>";
   echo $success;
