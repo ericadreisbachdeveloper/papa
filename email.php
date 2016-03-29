35c7be1b4811e6a94e655872b682991941fa8d07
erica dreisbach <erica@ericadreisbach.com>
2016-03-29 17:10:29 -0500

    added HTML email to portfolio

---
+++ email.php
@@ -0,0 +1,53 @@
+<?php $page = 'HTML Email Work'; include('header.php'); ?>
+
+<section>
+  <div class="wrapper page-top">
+    <h1 id="maincontent" class="page-title">Selected HTML email</h1>
+  </div><!-- .wrapper -->
+
+  <div class="wrapper">
+
+    <div class="email-cell responsive">
+      <h2 class="title">Sony </h2>
+      <a href="email/sony" class="link"><img class="img" src="img/email-thumb-sony.jpg" alt="Sony | responsive HTML email development by Erica Dreisbach" />
+      </a>
+    </div><!-- /.email-cell -->
+
+    <div class="email-cell responsive">
+      <h2 class="title">Budget </h2>
+      <a href="email/budget" class="link"><img class="img" src="img/email-thumb-budget.jpg" alt="Budget | responsive HTML email development by Erica Dreisbach" /></a>
+    </div><!-- /.email-cell -->
+
+    <div class="email-cell responsive">
+      <h2 class="title">Focus </h2>
+      <a href="email/focus" class="link"><img class="img" src="img/email-thumb-focus.jpg" alt="Focus email template | development by Erica Dreisbach" /></a>
+    </div><!-- /.email-cell -->
+
+    <div class="email-cell">
+      <h2 class="title">Announcement </h2>
+      <a href="email/announcement" class="link"><img class="img" src="img/email-thumb-announcement.jpg" alt="Announcement email template | design and development by Erica Dreisbach" /></a>
+    </div><!-- /.email-cell -->
+
+    <div class="email-cell">
+      <h2 class="title">Offer</h2>
+      <a href="email/offer" class="link"><img class="img" src="img/email-thumb-offer.jpg" alt="Offer | HTML email template by Erica Dreisbach" /> </a>
+    </div><!-- /.email-cell -->
+
+    <div class="email-cell">
+      <h2 class="title">Invitation</h2>
+      <a href="email/invitation" class="link"><img class="img" src="img/email-thumb-invitation.jpg" alt="Invitation | HTML email template by Erica Dreisbach" /></a>
+    </div><!-- /.email-cell -->
+
+  </div><!-- /.wrapper -->
+</section>
+
+<section>
+  <h2 class="hidden">Back to main portfolio</h2>
+  <div class="wrapper -txtcenter">
+    <p class="-bigpadding">
+      <a href="./" class="button">Main Portfolio </a>
+    </p>
+  </div><!-- /.wrapper -->
+</section>
+
+<?php include('footer.php'); ?>
