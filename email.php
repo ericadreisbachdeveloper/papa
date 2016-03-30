a2c734768bbd526af5f820644d21621d4cad7e0c
erica dreisbach <erica@ericadreisbach.com>
2016-03-30 14:07:20 -0500

    reworking email cell styling

--- email.php
+++ email.php
@@ -7,36 +7,35 @@
 
   <div class="wrapper">
 
-    <div class="email-cell responsive">
+    <a href="email/sony" class="link email-cell responsive">
       <h2 class="title">Sony </h2>
-      <a href="email/sony" class="link"><img class="img" src="img/email-thumb-sony.jpg" alt="Sony | responsive HTML email development by Erica Dreisbach" />
-      </a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-sony.jpg" alt="Sony | responsive HTML email development by Erica Dreisbach" />
+    </a>
 
-    <div class="email-cell responsive">
+    <a href="email/budget" class="link email-cell responsive">
       <h2 class="title">Budget </h2>
-      <a href="email/budget" class="link"><img class="img" src="img/email-thumb-budget.jpg" alt="Budget | responsive HTML email development by Erica Dreisbach" /></a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-budget.jpg" alt="Budget | responsive HTML email development by Erica Dreisbach" />
+    </a>
 
-    <div class="email-cell responsive">
+    <a href="email/focus" class="link responsive email-cell">
       <h2 class="title">Focus </h2>
-      <a href="email/focus" class="link"><img class="img" src="img/email-thumb-focus.jpg" alt="Focus email template | development by Erica Dreisbach" /></a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-focus.jpg" alt="Focus email template | development by Erica Dreisbach" />
+    </a>
 
-    <div class="email-cell">
+    <a href="email/announcement" class="link email-cell">
       <h2 class="title">Announcement </h2>
-      <a href="email/announcement" class="link"><img class="img" src="img/email-thumb-announcement.jpg" alt="Announcement email template | design and development by Erica Dreisbach" /></a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-announcement.jpg" alt="Announcement email template | design and development by Erica Dreisbach" />
+    </a>
 
-    <div class="email-cell">
+    <a href="email/offer" class="link email-cell">
       <h2 class="title">Offer</h2>
-      <a href="email/offer" class="link"><img class="img" src="img/email-thumb-offer.jpg" alt="Offer | HTML email template by Erica Dreisbach" /> </a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-offer.jpg" alt="Offer | HTML email template by Erica Dreisbach" />
+    </a>
 
-    <div class="email-cell">
+    <a href="email/invitation" class="link email-cell">
       <h2 class="title">Invitation</h2>
-      <a href="email/invitation" class="link"><img class="img" src="img/email-thumb-invitation.jpg" alt="Invitation | HTML email template by Erica Dreisbach" /></a>
-    </div><!-- /.email-cell -->
+      <img class="img" src="img/email-thumb-invitation.jpg" alt="Invitation | HTML email template by Erica Dreisbach" /></a>
+    </a>
 
   </div><!-- /.wrapper -->
 </section>
