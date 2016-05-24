b78dd845de1f1be9aee31fade317a0369c494f54
erica dreisbach <erica@ericadreisbach.com>
2016-05-24 16:54:17 -0500

    making that top frame display table

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 
 
 <section id="top" class="septagon parallax-window" data-parallax="scroll" data-image-src="img/spiders-web.jpg" data-class-name="septagon" style="min-height: 30em; height: calc(100vh - 50px); background-color: #ccc;">
-  <div class="container-fluid-width" style="position: relative; top: 50%; transform: translateY(-50%);">
-    <div class="container" style="opacity: .4; filter: alpha(opacity=40);">
-      <h1 class="-pagehead" style="color: white; line-height: 1; margin-bottom: 0; text-align: center; text-shadow: 1px 1px 3px #000;">erica dreisbach </h1>
-      <h2 class="-pagesubhead" style="color: white; font-style: italic; margin-top: .2em; text-align: center; text-shadow: 1px 1px 3px #000;">web designer +&nbsp;developer </h2>
+  <div class="container-fluid-width -displaytable">
+    <div class="container -displaytablecell " style="opacity: .4; filter: alpha(opacity=40);">
+        <h1 class="-pagehead" style="color: white; line-height: 1; margin-bottom: 0; text-align: center; text-shadow: 1px 1px 3px #000;">erica dreisbach </h1>
+        <h2 class="-pagesubhead" style="color: white; font-style: italic; margin-top: .2em; text-align: center; text-shadow: 1px 1px 3px #000;">web designer +&nbsp;developer </h2>
     </div>
   </div>
 </section>
