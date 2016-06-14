75ece9037b025dee7da9f8685fef3b234a2529c3
erica dreisbach <erica@ericadreisbach.com>
2016-06-14 15:43:18 -0500

    upped website launch count to 30! woot teamodea.com!

--- index.php
+++ index.php
@@ -212,7 +212,7 @@
         <div class="circle">
           <div class="-circlewrap">
             <span class="-circlesmalls">DEVELOPMENT LEAD ON</span><br />
-            <span class="-circleextrabigs">29</span><br />
+            <span class="-circleextrabigs">30</span><br />
             <span class="-circlesmalls">WEBSITES <span style="letter-spacing: 0; font-style: italic;">and counting!</span> </span>
           </div>
         </div>
