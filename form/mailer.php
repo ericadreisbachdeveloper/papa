8ec7c59bb6af0472aa1784c9f0da214bf02be842
erica dreisbach <erica@ericadreisbach.com>
2016-10-29 18:21:51 -0500

    moving working build of Ajax/PHP contact form

---
+++ form/mailer.php
@@ -0,0 +1,17 @@
+
+<?php
+    $to = "erica@ericadreisbach.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From:$name<$from>";
+    $subject = "web development project";
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"website"} = "website";
+    $fields{"message"} = "message";
+    $body = "Website: " . $_REQUEST['website'] . '
+
+    ' . $_REQUEST['message'];
+    $send = mail($to, $subject, $body, $headers);
+?>
