601b3f7424fa158cedc05baa498ea4e5753ac658
erica dreisbach <erica@ericadreisbach.com>
2017-03-10 22:15:27 -0600

    updates to websites

--- index.php
+++ index.php
@@ -260,7 +260,7 @@
         <div class="circle">
           <div class="-circlewrap">
             <span class="-circlesmalls">DEVELOPMENT LEAD ON</span><br />
-            <span class="-circleextrabigs">34</span><br />
+            <span class="-circleextrabigs">35</span><br />
             <span class="-circlesmalls">WEBSITES <span style="letter-spacing: 0; font-style: italic;">and counting!</span> </span>
           </div>
         </div>
