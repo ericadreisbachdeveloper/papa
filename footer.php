ee5e18ed78f7e3c5a1aae5d9f7581dd8ddcb6076
erica dreisbach <erica@ericadreisbach.com>
2018-01-28 09:05:41 -0600

    removing fingerprinting from FontAwesome css

--- footer.php
+++ footer.php
@@ -16,21 +16,6 @@
 
 
 
-<!-- Analytics - conditionally hid from PageSpeed Insights -->
-<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47393701-1', 'auto');
-  ga('send', 'pageview');
-</script>
-<?php endif; ?>
-
-
-
 <!-- jQuery -->
 <script src="https://s3.amazonaws.com/darkblack-papa/jquery.min.js"> </script>
 
@@ -157,11 +142,16 @@ $(document).ready(function(){
 
 
 <?php if($bodyclass == home) : ?>
-
 <!-- home cooked stuff -->
 <script src="https://s3.amazonaws.com/darkblack-papa/papa.min.js"></script>
-
 <?php endif; ?>
+
+
+
+<!-- Analytics - conditionally hid from PageSpeed Insights -->
+<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-47393701-1', 'auto'); ga('send', 'pageview');</script>
+<?php endif; ?>
+<!-- END Analytics -->
 
 
 
