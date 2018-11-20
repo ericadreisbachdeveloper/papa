dc68383d4936583b7c7ca12a6edab83940d22ea6
erica dreisbach <erica@ericadreisbach.com>
2018-11-20 16:44:23 -0600

    updates to privacy policy

---
+++ privacy/index.php
@@ -0,0 +1,48 @@
+<?php $bodyclass = 'page'; $page = 'Privacy Policy'; $metadescription = 'Privacy Policy | Chicago Web Developer Erica Dreisbach'; include('../header.php'); ?>
+
+
+
+<section id="maincontent" class="section">
+  <div class="container-fluid-width">
+    <div class="container">
+      <h1 class="-pagehead -txtcenter">Privacy Policy </h1>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="quotes-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2"> </div>
+
+      <div class="col-md-8 col-content">
+
+        <p class="p">This site uses Google Analytics to collect data about users and site traffic. You can read more about what Google collects and how they use it on their&nbsp;site: <a href="https://support.google.com/analytics/answer/6004245?hl=en" target="_blank" class="a"  rel="noopener">https://support.google.com/analytics/answer/6004245?hl=en</a></p>
+
+        <p class="p">This site does not use other cookies or similar tracking&nbsp;tools. </p>
+
+        <p class="p">Any information shared via this site's <a class="a" href="../#contact">contact form</a> will be kept confidential, barring explicit permission otherwise. In particular, if I'm not available for your project but can recommend a colleague, I will only share details of your project with such colleague upon receiving your explicit written&nbsp;consent. </p>
+
+      </div><!-- /.col-content -->
+
+      <div class="col-md-2"> </div>
+
+  </div><!-- /.container -->
+</section>
+
+
+
+<section class="button-section">
+  <h2 class="hidden">Navigation </h2>
+  <div class="container">
+    <p class="-bigpadding -txtcenter">
+      <a href="#" id="button" class="-ghost -naturalsize"> </a>
+    </p>
+  </div>
+</section>
+
+
+
+<?php include('../footer.php'); ?>
