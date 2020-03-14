c61f8a43bcd9d2bf8e37dffd17fb656c120ce367
erica dreisbach <erica@ericadreisbach.com>
2020-03-13 22:35:11 -0500

    50 websites and counting

--- index.php
+++ index.php
@@ -295,7 +295,7 @@
         <div class="circle">
           <div class="-circlewrap">
             <span class="-circlesmalls">DEVELOPMENT LEAD ON</span><br />
-            <span class="-circleextrabigs">49</span><br />
+            <span class="-circleextrabigs">50</span><br />
             <span class="-circlesmalls">WEBSITES<span style="letter-spacing: 0; font-style: italic;">and counting!</span> </span>
           </div>
         </div>
